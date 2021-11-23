86935c79725d1adf9585ef6b9273f89dff94e956
Falence Lemungoh <falencelemungoh@gmail.com>
2021-11-23 23:18:05 +0100

    convert decimal to any base algorithm

---
+++ decimal-to-any-base.js
@@ -0,0 +1,28 @@
+const Stack = require('./stack');
+
+// converts any decimal number to (2 <= base <= 36)
+function baseConverter({ decimalNumber, base }) {
+  const remainderStack = new Stack();
+  const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let number = decimalNumber;
+  let remainder;
+  let baseString = '';
+
+  if (!(base >= 2 && base <=36)) {
+    return '';
+  }
+
+  while (number > 0) {
+    remainder = number % base;
+    remainderStack.push(remainder);
+    number = Math.floor(number / base);
+  }
+
+  while (!remainderStack.isEmpty()) {
+    baseString += digits[remainderStack.pop()];
+  }
+
+  return baseString;
+}
+
+console.log(baseConverter({ base: 6, decimalNumber: 7 }));
