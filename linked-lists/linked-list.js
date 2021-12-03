8f8c44db2adfc4205e8142dc4f3ff7c3d8814837
Falence Lemungoh <falencelemungoh@gmail.com>
2021-12-03 07:57:20 +0100

    remove element in specific position in a linked list

--- linked-lists/linked-list.js
+++ linked-lists/linked-list.js
@@ -22,4 +22,25 @@ module.exports = class LinkedList {
     }
     this.count++;
   }
+
+  removeAt(index) {
+    // check for out of bound values
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        let previous;
+        for (let i = 0; i < index; i++) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+      }
+      this.count--;
+      return current.element
+    }
+    return undefined;
+  }
 }
